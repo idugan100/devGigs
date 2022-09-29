d9d29510b15014837d6901cc506db243cc8229c8
isaac <isaacd4444@gmail.com>
2022-09-29 11:23:18 -0400

    added hero and search partials

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,6 +2,8 @@
 
 
 @section('content')
+@include('partials._hero')
+@include('partials._search')
 <div
 class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
 >
@@ -16,12 +18,12 @@ class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
-            src="images/acme.png"
+            src="{{asset('images/no-image.png')}}"
             alt=""
         />
         <div>
             <h3 class="text-2xl">
-                <a href="show.html">{{$listing->title}}</a>
+                <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
             <ul class="flex">
